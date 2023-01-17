f94b37f58b37842c16c6dfad849684ff1a97246e
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-01-17 06:58:52 -0400

    a

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,19 @@
-console.log('Deu certo');
+import { PrismaClient } from '.prisma/client';
+import fastify from 'fastify';
+
+const app = fastify();
+const prisma = new PrismaClient();
+const port = 5000;
+
+app.get('/', () => {
+  const habits = prisma.habits.findMany({
+    where: { title: { statartsWith: 'M' } },
+  });
+  return 'Olá Mundo';
+});
+
+app
+  .listen({ port })
+  .then(() =>
+    console.log(`The server is running on: http://localhost:${port}`)
+  );
